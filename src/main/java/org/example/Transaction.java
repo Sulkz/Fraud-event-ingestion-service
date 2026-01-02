c4aa419a74109d4acbb902e7404eb7c5c0a1b688
Reuel Igbokwe <rcigbokweonuigbo1@sheffield.ac.uk>
2026-01-02 09:20:25 +0000

    Started doing Solo tasks from chat to increase skill and interviewability

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -7,7 +7,7 @@ public class Transaction {
     //attributes of a record
     private int transaction_id;
     private int user_id;
-    private double amount;
+    private double  amount;
     private String transaction_type;
     private String merchant_category;
     private double device_risk_score;
@@ -16,34 +16,31 @@ public class Transaction {
         ACCEPT, FLAG, REJECT 
 
     };
-    private Status status;
-    
-    //TODO Create a hashmap for the available transasction types
 
-    HashSet<String> transaction_types = new HashSet<>(Arrays.asList("ATM", "QR", "Online","POS"));
+    Status status;
     
-   
+    HashSet<String> transaction_types = new HashSet<>(Arrays.asList("ATM", "QR", "Online","POS"));
 
     // structual fields validation of each row
-    public void validationStructure(){
-        if (transaction_id <= 0 || user_id <= 0 || transaction_type == null || merchant_category == null || ip_risk_score <= 0 || device_risk_score <= 0  ){
-            this.status = Status.REJECT;
-            System.out.println("Reject");
-        }else if (amount == 0 || !transaction_types.contains(transaction_type)){
-            this.status = Status.FLAG;
-            System.out.println("Flag");
+    Status validationStructure(){
+        if (transaction_id <= 0 || user_id <= 0 || transaction_type == null
+             || merchant_category == null || ip_risk_score <= 0 || device_risk_score <= 0 
+             || !transaction_types.contains(transaction_type)){
+            return Status.REJECT;
+        }else if (amount == 0.0){
+            return Status.FLAG;
         }else{ 
-            this.status = Status.ACCEPT;
-            System.out.println("Accept");
+            return Status.ACCEPT;
         
         }
+
     }
 
 
     //getters
     public int get_transaction_id(){ return transaction_id;}
     public int get_user_id(){ return user_id;}
-    public double  get_amount(){ return amount;}
+    public double get_amount(){ return amount;}
     public String get_transaction_type(){ return transaction_type;}
     public double get_ip_risk_score(){ return ip_risk_score;}
     public double get_device_risk_score(){ return device_risk_score; }
